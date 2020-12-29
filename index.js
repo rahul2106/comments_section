83b9097158e22eacd9ddaf8eb4a21259b87fb210
rahul kumarcd <rahul.kmr2106@gmail.com>
2020-12-29 16:45:44 +0530

    Improvised function usibg arrayss.

--- index.js
+++ index.js
@@ -7,63 +7,65 @@ const comments={
 }
 
 var totalComments=0;
+var allComments=[];
 
-
-function add()
-{++totalComments;
+function addComment()
+{
+++totalComments;
 var c= Object.create(comments);
 c.id=totalComments;
 c.author="Rahul";
 c.comment=document.getElementById('newComment').value;
 c.reply={};
 console.log(c);
+allComments.push(c);
+console.log(allComments);
 createNewComment(c);
+}
+
+function deleteComment(id)
+{
+var list = document.getElementsByClassName('comments');
+console.log(list);
+list.removeChild(list.childNode[id]);
+allComments.splice(id-1,1);
+console.log(allComments);
+
+}
+
+function replyComment(id)
+{
+console.log(allComments[id-1]);
 }
 
 function createNewComment(c)
 {
   var newComment= document.createElement("P");
   newComment.innerHTML=c.comment;
+  newComment.class=totalComments;
   document.getElementById('comments').appendChild(newComment);
-  addBttn('comments');
+
   console.log(totalComments);
-
 }
-
-function del()
-{
-  var elems = document.getElementsByClassName('comments'),
-    elem;
-while(elems.length){
-    elem = elems.item(0);
-    elem.parentNode.removeChild(elem);
-}
-}
-
-
 
 function addBttn(id)
 {
   var del = document.createElement("BUTTON");
   del.innerHTML="Delete";
+  del.value=totalComments;
   del.addEventListener("click", function(){
-    var elems = document.getElementsByClassName('comments'),
-      elem;
-    while(elems.length){
-      elem = elems.item(0);
-      elem.parentNode.removeChild(elem);
-    }
-
+    return deleteComment(del.value);
   });
   document.getElementById(id).appendChild(del);
   var reply = document.createElement("BUTTON");
   reply.innerHTML= "Reply";
+  reply.value=totalComments;
   reply.addEventListener("click", function ()
   {
     var replyInput= document.createElement("INPUT");
     document.getElementById(id).appendChild(replyInput);
-    this..addBttn(id);
+
+   return replyComment(reply.value);
   });
   document.getElementById(id).appendChild(reply);
-
 }
